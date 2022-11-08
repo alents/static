d53172e18b943476f647004029d20812b0906bf5
alents <57786271+alents@users.noreply.github.com>
2022-11-08 12:55:44 +0500

    Update xss.test2.js

--- xss.test2.js
+++ xss.test2.js
@@ -1,9 +1,8 @@
-alert("xss.test2.js from outer file script");
+alert("xss.test2.js from outer file script with dynamic");
 test();
 
 function test() {
-    // let script = document.createElement('script');
-    // script.src = "/static/js/xsstest/xss.test3.js";
-    // document.getElementsByTagName('body')[0].appendChild(script);
-    eval("alert('eval - xss.test2.js')");
+    let script = document.createElement('script');
+    script.src = "https://alents.github.io/static/xss.test3.js";
+    document.getElementsByTagName('body')[0].appendChild(script);    
 }
